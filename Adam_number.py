f11b15e1b6d2cdf5c34ab9d3ed653ea8004289e5
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2023-05-22 15:30:08 +0530

    n=int(input()) m=n*n t=n rev=0 while(t!=0):     r=t%10     rev=rev*10+r     t=t//10 s=rev*rev rev1=0 while(s!=0):     r1=s%10     rev1=rev1*10+r1     s=s//10 print(m==rev1)

---
+++ Adam_number.py
@@ -0,0 +1,15 @@
+n=int(input())
+m=n*n
+t=n
+rev=0
+while(t!=0):
+    r=t%10
+    rev=rev*10+r
+    t=t//10
+s=rev*rev
+rev1=0
+while(s!=0):
+    r1=s%10
+    rev1=rev1*10+r1
+    s=s//10
+print(m==rev1)
